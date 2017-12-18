6ca4ee6851d79de33f606daf089fb2779b69caa0
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-18 14:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
+function happyHolidaysTo(holiday, name) {
+  return `Happy holidays, ${name}!`
+}
+
 function holidayCountdown() {
   
 }
